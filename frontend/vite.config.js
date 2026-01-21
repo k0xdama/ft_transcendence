5d2dd3daf4ba920e27cfcfba4233a09032722a91
morajaon <morajaon@student.42.fr>
2026-01-21 10:19:43 +0100

    Basic backend and frontend connection

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,5 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-	host: '0.0.0.0'},
+	  host: '0.0.0.0',
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://backend:5172',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
