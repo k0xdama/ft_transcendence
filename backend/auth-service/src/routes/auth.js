2b97ff8aec9a756f6f84b367c90fb9e42e873c07
abraga42 <86627490+abraga42@users.noreply.github.com>
2026-02-05 17:20:10 +0100

    login OK, simple logout OK, middleware to verify token OK

--- backend/auth-service/src/routes/auth.js
+++ backend/auth-service/src/routes/auth.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
 import { db } from '../db/queries.js';
+import { generateAccessToken } from '../utils/jwt.js';
+import { verifyToken } from '../middleware/verify-token.js';
 
 const router = express.Router();
 
@@ -24,13 +26,13 @@ router.post('/register', async (req, res) => {
 			return res.status(400).json({ error: 'This username is already taken' });
 		}
 
-		const password_hash = await bcrypt.hash(password, 10);
+		const passwordHash = await bcrypt.hash(password, 10);
 
 		const newUser = await db.one(
 			'INSERT INTO auth.users(email, username, password) VALUES($1, $2, $3) RETURNING id, email, username',
-			[email, username, password_hash]
+			[email, username, passwordHash]
 		);
-c
+
 		return res.status(201).json({
 			message: `${newUser.username}'s account has been successfully created!`,
 			user: newUser
@@ -40,6 +42,46 @@ c
 		console.error('Register: ', error);
 		res.status(500).json({ error: 'Internal Server Error'});
 	}
-})
+});
+
+router.post('/login', async (req, res) => {
+	try {
+		const { identifier, password } = req.body;
+
+		const user = await db.oneOrNone(
+			'SELECT id, email, username, password FROM auth.users WHERE email = $1 OR username = $1',
+			[identifier]
+		);
+		if (!user) {
+			return res.status(401).json({ error: 'User not found' });
+		}
+
+		const isValidPassword = await bcrypt.compare(password, user.password);
+		if (!isValidPassword) {
+			return res.status(401).json({ error: 'Invalid password' });
+		}
+
+		const accessToken = generateAccessToken(user);
+
+		return res.status(200).json({
+			message: 'Login successful',
+			user: { id: user.id, email: user.email, username: user.username },
+			accessToken
+		});
+	}
+	catch (error) {
+		console.error('Login: ', error);
+		res.status(500).json({ error: 'Internal Server Error'});
+	}
+});
+
+// token deletion on the client side
+router.post('/logout', async (req, res) => {
+	return res.status(200).json({ message: 'Logout successful' });
+});
+
+router.get('/test', verifyToken, (req, res) => {
+	return res.status(200).json({ user: req.user });
+});
 
 export default router;
