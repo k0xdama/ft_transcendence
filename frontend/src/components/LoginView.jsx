06b9acae560884820a435703952fdfac100b733b
morajaon <morajaon@student.42.fr>
2026-02-10 16:11:46 +0100

    added form handling in register and log in view. Ready to connect to Auth service

--- frontend/src/components/LoginView.jsx
+++ frontend/src/components/LoginView.jsx
@@ -1,17 +1,62 @@
 import { Link } from 'react-router-dom'
+import { use, useState } from 'react';
 import './LoginView.css'
 
 function LoginView() {
+	const [formData, setFormData] = useState({
+		username: '',
+		password: '',
+	})
+
+	const [error, setError] = useState('')
+	const [loading, setLoading] = useState(false)
+
+	const handleChange = (e) => {
+		setFormData({
+			...formData,
+			[e.target.name]: e.target.value
+		})
+	}
+
+	const handleLogin = async (e) => {
+		e.preventDefault()
+
+		if (!formData.password || !formData.username)
+		{
+			setError('Please enter username/email and password')
+			return
+		}
+
+		setLoading(true)
+
+		try {
+			//REPLACE BY AUTH SERVICE API CALL
+			await new Promise(resolve => setTimeout(resolve, 2000))
+			console.log('Log in successful!', {formData})
+		} catch (error) {
+			setError('Invalid username/email or password')
+		} finally {
+			setLoading(false)
+		}
+	}
+
 	return (
 		<div className='loginView'>
 			<h2 className='title'>Log in</h2>
+			{error && <div className='error'>{error}</div>}
 			<div className='inputs'>
 				<label>Username:</label>
-				<input type="text" className='inputField'/>
+				<input name= 'username' value={formData.username} onChange={handleChange} disabled={loading} type="text" className='inputField'/>
 				<label>Password:</label>
-				<input type="password" className='passInput'/>
+				<input name= 'password' value={formData.password} onChange={handleChange} disabled={loading} type="password" className='passInput'/>
 			</div>
-			<button className='comBut'>Log in</button>
+			<button className='comBut'
+			onClick={handleLogin}
+			disabled={loading}
+			>
+				{loading ? 'Logging in...' : 'Log in'}
+			</button>
+			<p>Don't have an account? <Link to="/register">Sign up</Link></p>
 		</div>
 	);
 }
--- frontend/src/components/RegisterView.jsx
+++ frontend/src/components/RegisterView.jsx
@@ -1,21 +1,77 @@
 import { Link } from 'react-router-dom'
+import { use, useState } from 'react';
 import './RegisterView.css'
 
 function RegisterView() {
+	const [formData, setFormData] = useState({
+		username: '',
+		email: '',
+		password: '',
+		confirmPassword:''
+	})
+	const [error, setError] = useState('')
+	const [loading, setLoading] = useState(false)
+
+	const handleChange = (e) => {
+		setFormData({
+			...formData,
+			[e.target.name]: e.target.value
+		})
+	}
+
+	const handleRegister = async (e) => {
+		e.preventDefault()
+		setError('')
+
+		if (formData.password !== formData.confirmPassword)
+		{
+			setError('Passwords do not match')
+			return
+		}
+		if (!formData.email.includes('@'))
+		{
+			setError('Email must be valid')
+			return
+		}
+		else
+		{
+			console.log('Validation passed ! Registration Data:', {formData})
+		}
+
+		setLoading(true)
+
+		try {
+			//REPLACE BY AUTH SERVICE API CALL
+			await new Promise(resolve => setTimeout(resolve, 2000))
+			console.log('Registration successful!')
+		} catch (error) {
+			setError('Resgistration Failed')
+		} finally {
+			setLoading(false)
+		}
+	}
+
 	return (
 		<div className='registerView'>
 			<h2 className='title'>Create a new account</h2>
+			{error && <div className='error'>{error}</div>}
 			<div className='inputs'>
 				<label>Username:</label>
-				<input type="text" className='inputField'/>
+				<input name= 'username' value={formData.username} onChange={handleChange} disabled={loading} type="text" className='inputField'/>
 				<label>Email address:</label>
-				<input type="text" className='inputField'/>
+				<input name= 'email' value={formData.email} onChange={handleChange} disabled={loading} type="text" className='inputField'/>
 				<label>Password:</label>
-				<input type="password" className='passInput'/>
+				<input name= 'password' value={formData.password} onChange={handleChange} disabled={loading} type="password" className='passInput'/>
 				<label>Confirm password:</label>
-				<input type="password" className='passInput'/>
+				<input name= 'confirmPassword' value={formData.confirmPassword} onChange={handleChange} disabled={loading} type="password" className='passInput'/>
 			</div>
-			<button className='comBut'>Register</button>
+			<button className='comBut'
+			onClick={handleRegister}
+			disabled={loading}
+			>
+				{loading ? 'Registering...' : 'Register'}
+			</button>
+			<p>Already have an account? <Link to="/login">Sign in</Link></p>
 		</div>
 	);
 }
