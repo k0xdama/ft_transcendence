924df0179cb47b0b295192cac1fcf937a382d14f
Anna BRAGA <annabrag@bess-f2r1s15.clusters.42paris.fr>
2026-02-16 19:53:29 +0100

    working on chat db tables

--- postgresql/init/01-init_tables.sql
+++ postgresql/init/01-init_tables.sql
@@ -21,60 +21,29 @@ GRANT EXECUTE ON FUNCTION update_updated_at() TO auth_user, player_user, lobby_u
 SET ROLE auth_user;
 
 CREATE TABLE auth.users (
-	id UUID			PRIMARY KEY DEFAULT gen_random_uuid(),
-	email			TEXT UNIQUE NOT NULL CHECK (length(email) <= 255 AND email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
-	username		TEXT UNIQUE NOT NULL CHECK (length(username) <= 50 AND username ~* '^[A-Za-z0-9_-]{3,}$'),
-	password_hash	TEXT NOT NULL CHECK (length(password_hash) <= 255),
+	id				UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+	email			TEXT UNIQUE NOT NULL
+		CHECK (length(email) <= 255 AND email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
+	username		TEXT UNIQUE NOT NULL
+		CHECK (length(username) BETWEEN 3 AND 50 AND username ~* '^[A-Za-z0-9_-]+$'),
+	password_hash	TEXT NOT NULL
+		CHECK (length(password_hash) <= 255),
 	-- is_verified		BOOLEAN DEFAULT FALSE,
 	-- is_active		BOOLEAN DEFAULT TRUE,
-	created_at		TIMESTAMPTZ DEFAULT NOW()
-	-- updated_at		TIMESTAMPTZ DEFAULT NOW()
+	created_at		TIMESTAMPTZ DEFAULT NOW(),
+	updated_at		TIMESTAMPTZ DEFAULT NOW()
 );
 
 -- Index for faster lookups
 CREATE INDEX idx_users_email ON auth.users(email);
 CREATE INDEX idx_users_username ON auth.users(username);
 
--- CREATE TRIGGER update_users_updated_at
--- 	BEFORE UPDATE ON auth.users
--- 	FOR EACH ROW
--- 	EXECUTE FUNCTION update_updated_at();
+CREATE TRIGGER update_users_updated_at
+	BEFORE UPDATE ON auth.users
+	FOR EACH ROW
+	EXECUTE FUNCTION update_updated_at();
 
 RESET ROLE;
-
-
--- ==============================
--- PLAYER SCHEMA
--- ==============================
-
-SET ROLE player_user;
-
--- Tables creation + trigger here...
-
-RESET ROLE;
-
-
--- ==============================
--- LOBBY SCHEMA
--- ==============================
-
-SET ROLE lobby_user;
-
--- Tables creation + trigger here...
-
-RESET ROLE;
-
-
--- ==============================
--- GAME SCHEMA
--- ==============================
-
-SET ROLE game_user;
-
--- Tables creation + trigger here...
-
-RESET ROLE;
-
 
 -- ==============================
 -- CHAT SCHEMA
@@ -83,8 +52,14 @@ RESET ROLE;
 SET ROLE chat_user;
 
 CREATE TABLE chat.messages (
-	id UUID			PRIMARY KEY DEFAULT gen_random_uuid(),
-
+	id				UUID PRIMARY KEY,
+	lobby_id		UUID NOT NULL,
+	sender_id		UUID NOT NULL,
+	content			TEXT NOT NULL,
+	message_type	VARCHAR(20) NOT NULL DEFAULT 'user-text', -- ou predefined, system
+	game_id			UUID, -- NULL si pré-game, rempli si in-game
+	game_phase		VARCHAR(20), -- 'lobby', 'playing', 'post-game'
+	created_at		TIMESTAMP DEFAULT NOW()
 );
 
 RESET ROLE;
