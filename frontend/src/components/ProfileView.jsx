9e380433bac55652e9618eea66d41d7b1b861193
morajaon <morajaon@student.42.fr>
2026-02-23 15:16:17 +0100

    restructured file tree in src for better navigation

--- frontend/src/components/ProfileView.jsx
+++ frontend/src/components/ProfileView.jsx
@@ -1,7 +1,6 @@
 import { Link, useNavigate } from 'react-router-dom'
 import { useState } from 'react';
 import { useAuth } from '../context/AuthContext';
-import './LoginView.css'
 
 function ProfileView() {
 	
