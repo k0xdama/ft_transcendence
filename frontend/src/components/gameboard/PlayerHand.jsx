e0e0378f22b52517fe2c50477078922acf712947
morajaon <morajaon@student.42.fr>
2026-02-27 15:38:40 +0100

    added card images to board

--- frontend/src/components/gameboard/PlayerHand.jsx
+++ frontend/src/components/gameboard/PlayerHand.jsx
@@ -1,9 +1,20 @@
+const	cardImages = import.meta.glob('../../assets/cards/Card_*.png', { eager: true })
+
+const	getCardImage = (label) => {
+	const	key = `../../assets/cards/Card_${label}.png`
+	return	cardImages[key]?.default
+}
+
 function PlayerHand({ cards, seat }) {
 	return (
 		<div className={`player-hand ${seat}`}>
 			{cards.map(card => (
 				<div key={card.id} className="card card-front">
-					{card.label}
+					<img
+						src={getCardImage(card.label)}
+						alt={`Card ${card.label}`}
+						className="card-img"
+					/>
 				</div>
 			))}
 		</div>
