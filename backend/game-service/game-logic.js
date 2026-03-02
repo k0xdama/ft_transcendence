223114530f2d35e757f26fab03d0e7045be036b5
u4s2e0r <prioux.mateo@outlook.com>
2026-03-02 23:17:06 +0100

    New branch game-service

---
+++ backend/game-service/game-logic.js
@@ -0,0 +1,47 @@
+export function createDeck() {
+
+}
+
+export function createGame(players) {
+
+}
+
+export function getHighestCard(cards) {
+
+}
+
+export function getLowestCard(cards) {
+
+}
+
+export function flipMiddleCard(state, position) {
+
+}
+
+export function removeCard(state, cardID) {
+
+}
+
+export function shuffleAndDistribute(deck, players) {
+
+}
+
+export function executeAction(state, action) {
+
+}
+
+export function nextPlayer(state) {
+
+}
+
+export function checkForPair(cards) {
+
+}
+
+export function checkForTrio(cards) {
+
+}
+
+export function checkWinConditions(state) {
+	
+}
