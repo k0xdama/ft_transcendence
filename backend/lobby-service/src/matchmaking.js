4fbab9cd2b2c5d9181adb08b779340401ea26796
u4s2e0r <prioux.mateo@outlook.com>
2026-03-22 03:31:46 +0100

    matchmaking done, need to test, reconnexion missing

---
+++ backend/lobby-service/src/matchmaking.js
@@ -0,0 +1,36 @@
+import { GAME_MODES, GAME_TYPES } from '../lobby-service.js';
+//la map se compose comme suit : key [MODE-TYPE-NOMBREDEJOUEURS] -> value [object: socket.user.id & socket]
+//exemple : CLASSIC/LINKED-SOLO/TEAM_UP-3/4/5/6
+
+
+
+export const queues = new Map();
+export const queueBySocket = new Map();
+
+function generateCompositeKey() {
+	
+}
+
+export function joinQueue(socket, data) {
+	const queueKey = generateCompositeKey(data);
+	if (!queues.has(queueKey))
+		queues.set(queueKey, []);
+	const queue = queues.get(queueKey);
+	const user = {
+		userId: socket.user.id,
+		socket : socket
+	}
+	queue.push(user);
+	queueBySocket.set(socket.id, queueKey);
+	if (queue.length === data.maxUsers) {
+		const matchedPlayers = queue.splice(0);
+		for (const user of matchedPlayers)
+   			queueBySocket.delete(user.socket.id);
+		return matchedPlayers;
+	}
+	return null;
+}
+
+function checkMatch() {
+
+}
