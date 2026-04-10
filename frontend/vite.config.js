0656e7112861b85d251ea8901d5920814d465c08
k0xdama <prioux.mateo@outlook.com>
2026-04-11 00:16:36 +0200

    Nginx added -> main port changed to 8443 + Dockerfile adapted using multi-stage builds + adapted vite config for dev+ nginx server configuration file default.conf added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,19 +2,21 @@ import { defineConfig } from 'vite'
 import fs from 'fs';
 import react from '@vitejs/plugin-react'
 
+const httpsConfig = fs.existsSync('../secrets/key.pem') ? {
+  key: fs.readFileSync('../secrets/key.pem'),
+  cert: fs.readFileSync('../secrets/cert.pem'),
+} : undefined
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
 	  host: '0.0.0.0',
     port: 5173,
-    https: {
-        key: fs.readFileSync('/run/secrets/ssl_key'),
-        cert: fs.readFileSync('/run/secrets/ssl_cert'),
-    },
+    https: httpsConfig,
     proxy: {
       '/api': {
-        target: 'https://gateway:4000',
+        target: 'https://localhost:4000',
 	    	ws: true,
         changeOrigin: true,
         secure: false
